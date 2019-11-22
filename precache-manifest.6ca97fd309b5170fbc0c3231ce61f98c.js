c975b1a0b2bb9a3c918da941c1e1a3f565623f8e
Klaudia <klaudia.krzywda@gmail.com>
2019-11-22 21:22:04 +0000

    Updates

---
+++ precache-manifest.6ca97fd309b5170fbc0c3231ce61f98c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ee5e2ef72ea075523f5fd13f01eb15",
+    "url": "/FavNote/index.html"
+  },
+  {
+    "revision": "4875db66f3dfb36f6110",
+    "url": "/FavNote/static/css/main.db49797f.chunk.css"
+  },
+  {
+    "revision": "ed4f38a9dd8b8125ed9c",
+    "url": "/FavNote/static/js/2.5cee3a10.chunk.js"
+  },
+  {
+    "revision": "4875db66f3dfb36f6110",
+    "url": "/FavNote/static/js/main.5becf6b7.chunk.js"
+  },
+  {
+    "revision": "3f21390ee48bf86d1b93",
+    "url": "/FavNote/static/js/runtime-main.34f1ff74.js"
+  },
+  {
+    "revision": "1e47eba9044a9158f9bddab834f075e2",
+    "url": "/FavNote/static/media/logo.1e47eba9.svg"
+  }
+]);
