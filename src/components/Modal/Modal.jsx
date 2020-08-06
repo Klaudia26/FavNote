1eac5cfd298636deeb72fce377e26b9cd7baa8f9
Klaudia Krzywda <klaudia.krzywda@gmail.com>
2020-08-06 21:34:17 +0100

    change the text @Klaudia26

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './Modal.module.scss';
-import Form from '../Form/Form';
+import React from "react";
+import styles from "./Modal.module.scss";
+import Form from "../Form/Form";
 
 const Modal = ({ closeModalFn }) => (
   <>
